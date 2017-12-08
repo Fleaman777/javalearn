0e8b4ed54f0f578247f66329e62b43dbc9e5e87c
Fleaman777 <fleaman777@gmail.com>
2017-12-08 19:48:27 +0300

    cycle degree

--- src/method.java
+++ src/method.java
@@ -1,11 +1,21 @@
 public class method {
-    static int degree(int a, int b) {
-        return (a );
-    }
-    public static void main(String[] args) {
-        int a=11;
-        System.out.println(method(a));
 
-        System.out.println(method(a)+ 2);
+    static int degree(int a, int b){
+
+        int solve=a;
+        for (int i=1; i<b; i++){
+
+            solve*=a;
+        }
+        System.out.println(solve);
+
+        return solve;
+
+    }
+
+    public static void main(String[] args) {
+
+        degree(5,3);
+
     }
 }
