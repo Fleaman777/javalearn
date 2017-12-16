7f90292527b78c149a3125ad212a263a80c80b32
Fleaman777 <fleaman777@gmail.com>
2017-12-16 14:47:27 +0300

    Revert "create a test package"
    This reverts commit b93fe24

---
+++ src/method.java
@@ -0,0 +1,23 @@
+public class method {
+
+    static int degree(int a, int b){
+
+        int solve=a;
+        for (int i=1; i<b; i++){
+
+            solve*=a;
+        }
+        System.out.println(solve);
+
+        return solve;
+
+    }
+
+    public static void main(String[] args) {
+
+        degree(5,3);
+
+        fghfg
+                //gfgf
+    }
+}
---
+++ src/popa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+class popa {
+    public static void main(String[] args) {
+
+        System.out.println("vvedite frazy=u");
+
+        Scanner fraz=new Scanner(System.in);
+        String vv=fraz.nextLine();
+
+        System.out.println(vv);
+
+
+
+
+
+    }
+}
