e3bdbcadf857d30d28618e0929b5ba4c8c53cd81
Fleaman777 <fleaman777@gmail.com>
2017-12-24 15:17:45 +0300

    implementation of the method that considers the factorial of a number in the usual and recursive way. And other examples

---
+++ src/Factorial.java
@@ -0,0 +1,36 @@
+public class Factorial {
+
+//    static int Fact=1;
+//
+//    static int solve(int number){
+//
+//        for (int i=1; i<=number; i++){
+//
+//            Fact = i*Fact;
+//        }
+//        return Fact;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//       int mult = solve(6);
+//
+//        System.out.println(mult);
+//    }
+//}
+
+    static int FactRecurs(int number){
+
+        if (number == 1) return number;
+        return (number -1)*number;
+
+    }
+
+    public static void main(String[] args) {
+        int num=4;
+        for (int i = 1; i<=num; i++){
+            System.out.println(i + "!" + " = " +FactRecurs(i));
+        }
+    }
+
+}
---
+++ src/example.java
@@ -0,0 +1,86 @@
+// class Example {
+//
+//    void Example(){
+//
+//        for (int i =0; i<5; i++){
+//            if (i==2) return;
+//            System.out.println("what is it?");
+//        }
+//        System.out.println("example example");
+//    }
+//
+//    public static void main(String[] args) {
+//        Example book = new Example();
+//        book.Example();
+//    }
+//}
+//
+//
+// class Vehicle{
+//     int wheels;
+//
+//
+//
+//    Vehicle (int wheelss, int pasengers, int speed, int weight){
+//         wheels=wheelss;
+//
+//    }
+//
+//     public static void main(String[] args) {
+//
+//        Vehicle car = new Vehicle(4,4,200,1000);
+//        Vehicle bus = new Vehicle(6,30,100,5000);
+//
+//         System.out.println(car.wheels);
+//
+//
+//     }
+//
+//
+//    }
+
+
+//class buy{
+//        void pay(int money){
+//            System.out.println("you pay product by money: " + money);
+//    }
+//        void pay (String Card){
+//            System.out.println(" ypu pay product by card" + "number card: " + Card);
+//        }
+//}
+//
+//class ByBuy{
+//
+//    public static void main(String[] args) {
+//
+//        buy banana = new buy();
+//        buy apple = new buy();
+//
+//        apple.pay("1234 1234 124 1243");
+//
+//        banana.pay(100);
+//    }
+//}
+
+
+//class Lego{
+//
+//    int block;
+//    int bridge;
+//    int people;
+//
+//    Lego(){
+//        this(3,4,1,"Uzi");
+//    }
+//
+//    Lego(int block, int bridge, int people, String gun){
+//
+//        this.block = block;
+//        this.bridge = bridge;
+//        this.people = people;
+//    }
+//}
+
+
+
+
